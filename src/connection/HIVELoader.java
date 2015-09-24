77ccd85bfcec708d02af4b314d3f5c2762678448
zx <727947094@qq.com>
2015-09-24 16:57:27 +0800

    更新1

--- src/connection/HIVELoader.java
+++ src/connection/HIVELoader.java
@@ -6,15 +6,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
-
 public class HIVELoader {
-	private static String url = 
-			"jdbc:hive2://192.168.0.11:10000/default;autho=NOSASL";
-	private static String username ="hive";
-	private static String pwd="hive";
-	private static String drivername ="org.apache.hive.jdbc.HiveDriver";
-	private static String sql = "select * from test_zx limit 10";
+	private static String url = "jdbc:hive2://192.168.0.61:10000";
+	private static String username = "root";
+	private static String pwd = "root";
+	private static String drivername = "org.apache.hive.jdbc.HiveDriver";
+	private static String sql = "select * from testforzx limit 10";
+
 	public static void main(String[] args) {
 		HIVELoader hiveloader = new HIVELoader();
 		try {
@@ -22,40 +20,40 @@ public class HIVELoader {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	public void exehive(String url,
-			String username,String pwd,String drivername,String sql) 
-					throws ClassNotFoundException{
+
+	public void exehive(String url, String username, String pwd,
+			String drivername, String sql) throws ClassNotFoundException {
 		Class.forName(drivername);
 		Connection conn = null;
 		Statement stat = null;
 		ResultSet rst = null;
-		if(conn == null){
+		if (conn == null) {
 			try {
-				conn = DriverManager.getConnection(url,username,pwd);
-				stat =  conn.createStatement();
+				conn = DriverManager.getConnection(url, username, pwd);
+				stat = conn.createStatement();
 				rst = stat.executeQuery(sql);
-				while(rst.next()){
+				while (rst.next()) {
 					System.out.println(rst.getString(2));
-					System.out.println(rst.getString("test_zx.a_iden_string"));
+					// System.out.println(rst.getString("test_zx.a_iden_string"));
 				}
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}finally{
-				try {
-					stat.close();
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				try {
-					conn.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+			} finally {
+				// try {
+				// stat.close();
+				// } catch (SQLException e1) {
+				// // TODO Auto-generated catch block
+				// e1.printStackTrace();
+				// }
+				// try {
+				// conn.close();
+				// } catch (SQLException e) {
+				// // TODO Auto-generated catch block
+				// e.printStackTrace();
+				// }
 			}
 		}
 	}
---
+++ src/connection/HiveClient.java
@@ -0,0 +1,61 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class HiveClient {
+
+	private static String url = "jdbc:hive2://192.168.1.61:10000/default";
+	private static String username = "root";
+	private static String pwd = "root";
+	private static String drivername = "org.apache.hive.jdbc.HiveDriver";
+	private static String sql = "select * from testforzx limit 10";
+
+	public static void main(String[] args) {
+		HiveClient hiveloader = new HiveClient();
+		try {
+			hiveloader.exehive(url, username, pwd, drivername, sql);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void exehive(String url, String username, String pwd,
+			String drivername, String sql) throws ClassNotFoundException {
+		Class.forName(drivername);
+		Connection conn = null;
+		Statement stat = null;
+		ResultSet rst = null;
+		if (conn == null) {
+			try {
+				conn = DriverManager.getConnection(url, username, pwd);
+				stat = conn.createStatement();
+				rst = stat.executeQuery(sql);
+				while (rst.next()) {
+					System.out.println(rst.getString(2));
+					// System.out.println(rst.getString("test_zx.a_iden_string"));
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				// try {
+				// stat.close();
+				// } catch (SQLException e1) {
+				// // TODO Auto-generated catch block
+				// e1.printStackTrace();
+				// }
+				// try {
+				// conn.close();
+				// } catch (SQLException e) {
+				// // TODO Auto-generated catch block
+				// e.printStackTrace();
+				// }
+			}
+		}
+	}
+}
